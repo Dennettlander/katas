f2559551e1d937f46fb54aeedf24467c584ae1bf
dennettlander <dennettlander@hotmail>
2016-07-22 10:43:49 +0200

    Añadida posibilidad de repetir el juego

--- src/FizzBuzz/FizzBuzzCommand.php
+++ src/FizzBuzz/FizzBuzzCommand.php
@@ -3,9 +3,12 @@
 namespace FizzBuzz;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\Question;
 
 
 class FizzBuzzCommand extends Command
@@ -38,19 +41,31 @@ class FizzBuzzCommand extends Command
         $jugada = $input->getArgument('jugada');
         $nombre = $input->getOption('jugador');
         $idioma = $input->getOption('idioma');
+        $jugar = true;
 
-        try {
-            $fbg = new FizzBuzzGame(Translator::createForLanguage($idioma));
-            $resultados = $fbg->playUpTo($jugada);
+        /** @var QuestionHelper $helper */
+        $helper = $this->getHelper('question');
+        $repetir = new ConfirmationQuestion('¿Desea repetir? (s/n) ', false, '/^s/i');
+        $numeroAUsar = new Question('¿Qué número desea usar? ', 'a');
 
-            foreach ($resultados as $resultado)
-            {
-                $output->writeln($resultado);
+        while (true === $jugar) {
+            try {
+                $fbg = new FizzBuzzGame(Translator::createForLanguage($idioma));
+                $resultados = $fbg->playUpTo($jugada);
+
+                foreach ($resultados as $resultado) {
+                    $output->writeln($resultado);
+                }
+            } catch (\InvalidArgumentException $e) {
+                $output->writeln($e->getMessage());
             }
 
-            $output->writeln('Hasta la proxima ' . $nombre);
-        } catch (\InvalidArgumentException $e) {
-            $output->writeln($e->getMessage());
+            if ($helper->ask($input, $output, $repetir)){
+                $jugada = $helper->ask($input, $output, $numeroAUsar);
+            }else{
+                $jugar = false;
+            }
         }
+        $output->writeln('Hasta la proxima, ' . $nombre);
     }
 }
